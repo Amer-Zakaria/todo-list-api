61e2fde377ab78a51adf136712e9b0edc83872cc
Amer-Zakaria <ameruniversity1234@gmail.com>
2024-03-18 17:38:57 +0300

    Add Google OAuth redirect URI to production

--- config/development.js
+++ config/development.js
@@ -4,4 +4,7 @@ module.exports = {
   accessTokenTtl: 5 * 60, // 5min
   origin: "http://localhost:3000",
   jwtPrivateKey: "1234",
+  google: {
+    redirectUri: "http://localhost:3001/api/users/oauth/google",
+  },
 };
--- config/production.js
+++ config/production.js
@@ -3,4 +3,8 @@ module.exports = {
   accessTokenTtl: 1 * 60 * 60, //1h
   origin: "https://todo-list-ui.netlify.app",
   stack: false,
+  google: {
+    redirectUri:
+      "https://todo-list-api-ml1b.onrender.com/api/users/oauth/google",
+  },
 };
